4d9c0d8c9afed6a11e5ae524a1f59cd869205f4a
Rafael Gallo <rafaelhenriquegallo@gmail.com>
2020-03-04 09:00:27 -0300

    Desafios aulas

---
+++ Banco de dados/tabela - 1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE(nome text, telefone text);
+.TABLE
+CREATE TABLE(marticulas integer, nome texto, telefone text, cidade text);
+.TABLE
+CREATE TABLE(produtos integer, quantidade text);
+.TABLE
